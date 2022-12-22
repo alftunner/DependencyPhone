a824c127dc68c5d38e233acfabcb440d91a74864
alftunner <alftunner@gmail.com>
2022-12-22 19:18:28 +0300

    Initial commit

---
+++ DependencyPhone/Phone.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+
+namespace DependencyPhone;
+
+public class Phone : DependencyObject
+{
+    public static readonly DependencyProperty NameProperty;
+    public static readonly DependencyProperty PriceProperty;
+
+    static Phone()
+    {
+        NameProperty = DependencyProperty.Register("Name", typeof(string), typeof(Phone));
+        FrameworkPropertyMetadata metadata = new FrameworkPropertyMetadata();
+        metadata.CoerceValueCallback = new CoerceValueCallback(CorrectValue);
+        PriceProperty = DependencyProperty.Register("Price", typeof(int), typeof(Phone), metadata, new ValidateValueCallback(ValidateValue));
+    }
+
+    private static object CorrectValue(DependencyObject d, object baseValue)
+    {
+        int currentValue = (int)baseValue;
+        if (currentValue > 5000)
+        {
+            return 5000;
+        }
+
+        if (currentValue < 0)
+        {
+            return Math.Abs(currentValue);
+        }
+
+        return currentValue;
+    }
+
+    private static bool ValidateValue(object value)
+    {
+        return true;
+    }
+
+    public string Name
+    {
+        get { return (string)GetValue(NameProperty); }
+        set { SetValue(NameProperty, value); }
+    }
+    
+    public int Price
+    {
+        get { return (int)GetValue(PriceProperty); }
+        set { SetValue(PriceProperty, value); }
+    }
+}
